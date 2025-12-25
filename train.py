66f20ce9ac93e797c84a383914f9a6ff66d46ef7
Avaish Alam <avaishshaikh49@gmail.com>
2025-12-25 22:11:38 +0530

    Dec tree model

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load dataset
@@ -12,7 +12,7 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
-model = LogisticRegression(max_iter=200)
+model = DecisionTreeClassifier(max_iter=200)
 model.fit(X_train, y_train)
 
 
